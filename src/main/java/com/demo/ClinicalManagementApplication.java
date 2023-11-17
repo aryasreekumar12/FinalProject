93a7bcdb18f6e3b0304a49be3583fdb37e961858
Reshma2295 <reshmaantony2295@gmail.com>
2023-11-17 14:39:53 +0530

    my last commit

--- src/main/java/com/demo/ClinicalManagementApplication.java
+++ src/main/java/com/demo/ClinicalManagementApplication.java
@@ -8,5 +8,6 @@ public class ClinicalManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicalManagementApplication.class, args);
+		System.out.println("hi world");
 	}
 }
